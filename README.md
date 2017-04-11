9dcdaa2f7a9d43f01ff1c3f7a30214013313af72
Mateusz Adamiec <adamiecmateusz@wp.pl>
2017-04-11 10:49:06 +0200

    adding test upload api

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # mad_img
-API for uploading images
+API to upload images to AMAZON S3 and saving link, id and parameters on mongoDB
+
+## Instalation guide  
+####Installing on localhost 
+1. Have to set three environment variables :
+```
+AWS_ACCESS_KEY_ID
+```
+,
+```
+AWS_SECRET_ACCESS_TOKEN
+```
+,
+```
+MAD_IMG_BUCKET
+```,
+```
+MAD_IMG_MONGO
+```.
+mad.img.bucket is a bucket name, where images will be upload. mad.img.mongo is a mongodb address, where images will be upload, collection name should be created and name 'images': f.e. mongodb://localhost:27017/madimg.
+2. Wget project from github: ```https://github.com/Svaigas/mad_img.git```
+3. Go to directory, where you download zip, unpack and do : ```npm install```
+4. Run by using command :```npm run```
+
+
+####Installing using Dockerfile
+-- TBD
+
+## How to call API
+POSTMAN call example
+
+![](https://raw.githubusercontent.com/Svaigas/mad_img/master/docs/Request1.JPG?token=AZqEMWJwRa3TvEyTnCbrB08p4EsvdBLdks5Y9dJkwA%3D%3D)
+
+Data is returned in JSON format. Sample below:
+
+	[{"url":"https://mad.img.bucket.s3.amazonaws.com/rkokvf9ag_320_640.jpg","width":320,"height":640,"_id":"58ec96e2496a280cb814036c"},{"url":"https://mad.img.bucket.s3.amazonaws.com/rkokvf9ag_600_600.jpg","width":600,"height":600,"_id":"58ec96e2496a280cb814036d"}]
+
+
+
--- test/test-upload.js
+++ test/test-upload.js
@@ -2,6 +2,8 @@ var Code = require('code')
 var Glue = require('glue')
 var Fs = require('fs')
 var Path = require('Path')
+var FormData = require('form-data')
+var streamToPromise = require('stream-to-promise')
 
 var Lab = require('lab')
 var lab = exports.lab = Lab.script()
@@ -13,7 +15,7 @@ var it = lab.test
 
 var manifest = null
 var server = null
-var prefix = '/images/v1'
+var prefix = '/image/v1'
 
 before((done) => {
 	var manifestFile = Path.join(__dirname, 'manifest.json')
@@ -30,10 +32,30 @@ before((done) => {
 })
 
 describe('Upload.js: @testing', function() {
+	it('S123431242n', (done) => {
 
-	it('returns true when 1 + 1 equals 2', (done) => {
+		var image = {
+			filedata: Fs.readFileSync(Path.join(__dirname, '1920x1080test.jpg')).toString('base64'),
+			sizeList: '[{"width":320,"height":640},{"width":600,"height":600}]'
+		};
 
-		Code.expect(1 + 1).to.equal(2);
-		done();
-	});
-});
+		var form = new FormData();
+
+		// Fill the form object
+		Object.keys(image).forEach(function(key) {
+			form.append(key, image[key]);
+		});
+		streamToPromise(form).then(function(payload) {
+			server.inject({
+				method: 'POST',
+				url: prefix + '/uploadImages/metadata',
+				headers: form.getHeaders(),
+				payload: payload
+			}, (res) => {
+				expect(res.statusCode).to.equal(200);
+				done();
+			})
+		})
+	})
+
+})
